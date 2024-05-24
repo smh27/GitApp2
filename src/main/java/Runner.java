30e0fabcef2dacbb9aee7c3789c6d9e6fc931449
Semih Öztekin <oztekinsemih27@gmail.com>
2024-05-24 23:33:52 +0300

    yeniV1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,11 +2,5 @@ public class Runner {
     public static void main(String[] args) {
         //version 1
         System.out.println("hello GIT");
-
-        //version 2
-        System.out.println("git commit ");
-
-        //version 3
-        System.out.println("geçmişe yolculuk");
     }
 }
