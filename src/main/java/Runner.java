11f36a25ceb9dbc13666ee82a6ef2c0f57c71ef3
Semih Öztekin <oztekinsemih27@gmail.com>
2024-05-25 20:48:33 +0300

    v4

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,10 @@ public class Runner {
     public static void main(String[] args) {
         //version 1
         System.out.println("hello GIT");
+        System.out.println("git add . : staging areaya alir.");
+
+        //version 4
+        System.out.println("hi");
+
     }
 }
